eb954b5466aed2d9c12e4d3c3a2f41f871370fce
MathiasHareide <mathias.hareide4497@gmail.com>
2018-02-12 19:27:34 +0100

    Added first person view, and movement controls
    Player looks around with mouse, and move using either WASD or the arrow keys.

---
+++ Specific Pacific/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float HorizontalSpeed;
+    public float VerticalSpeed;
+
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * HorizontalSpeed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * VerticalSpeed;
+
+        transform.Translate(x, 0, z);
+    }
+}
