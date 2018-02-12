58bec8335895652f52df0f15cbf27a213582f436
MathiasHareide <mathias.hareide4497@gmail.com>
2018-02-12 20:12:20 +0100

    Added jump
    The player can jump when he is grounded.

--- Specific Pacific/Assets/Scripts/PlayerController.cs
+++ Specific Pacific/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,27 @@ public class PlayerController : MonoBehaviour
 
     public float HorizontalSpeed;
     public float VerticalSpeed;
+    public float jumpForce;
+
+    private Rigidbody rb;
+    public bool grounded;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        grounded = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        grounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        grounded = false;
+    }
 
     void Update()
     {
@@ -12,5 +33,12 @@ public class PlayerController : MonoBehaviour
         var z = Input.GetAxis("Vertical") * Time.deltaTime * VerticalSpeed;
 
         transform.Translate(x, 0, z);
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+            grounded = false;
+            Debug.Log("Jump");
+        }
     }
 }
